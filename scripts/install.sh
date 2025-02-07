3e2495db58e4e03d3995339bb91843f340d6c0bb
ElieGershom <eli.gershom@whitesourcesoftware.com>
2025-02-07 22:26:55 +0200

    fix script port-forward

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,24 @@
 kind create cluster --name fundguard
 
 # install es-oprator helm chart
+echo installing elastic-operator...
 helm install elastic-operator ./charts/eck-operator --namespace elastic-stack --create-namespace
 
 # install elastic-search-cluster helm chart
+echo installing elastic-search-cluster...
 helm install elastic-search-cluster ./charts/elastic-search-cluster --namespace elastic-stack
 
-# print out elastic-search-cluster url
-ELASTIC_SEARCH_URL=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' fundguard-control-plane):3000
-echo $ELASTIC_SEARCH_URL
+# port-forward elastic-search-cluster
+echo port-forward elastic-search-cluster to local network...
+
+retry_count=0
+max_retries=10
+until kubectl port-forward service/elastic-search-cluster-es-http 9200:9200 --namespace elastic-stack; do
+    retry_count=$((retry_count + 1))
+    if [ $retry_count -ge $max_retries ]; then
+        echo "Failed to port-forward after $max_retries attempts."
+        exit 1
+    fi
+    echo "Retrying... ($retry_count/$max_retries)"
+    sleep 15
+done
